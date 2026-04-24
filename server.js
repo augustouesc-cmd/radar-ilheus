5e9e6eabc7ceb7162ba3a8928846d6950437349b
augustouesc-cmd <augustouesc@yahoo.com.br>
2026-04-23 21:09:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,14 @@ const path = require("path");
 
 const app = express();
 
-// Porta do Railway (OBRIGATÓRIO)
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos (HTML, CSS, JS)
 app.use(express.static(__dirname));
 
-// Rota principal
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
